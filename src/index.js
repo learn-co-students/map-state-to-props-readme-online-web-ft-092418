f49a82bab0a3a5a05bea6b73a678b0153a96c1f7
github username <github email address>
2019-02-28 16:47:56 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'; //needed to use the Provider component
+
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
@@ -11,6 +13,11 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <App store={store}/>,
+  //Provider component comes from React Redux library -- alerts Redux app when there's a change to state && will render our React app
+  //pass store instance as a prop to make it available to all other components
+  <Provider store={store}> 
+
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
